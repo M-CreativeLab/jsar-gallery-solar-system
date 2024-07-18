5e18f2345fa84b1a5e5385eec6bc63f8e4179818
Yorkie Makoto <yorkiefixer@gmail.com>
2024-07-18 17:29:38 +0800

    Update main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -2,7 +2,7 @@ spatialDocument.addEventListener('spaceReady', () => {
   const scene = spaceDocument.scene as BABYLON.Scene;
   const model = scene.getNodeById('model');
   model.getChildMeshes().forEach(mesh => {
-    if (mesh.material.getClassName() === 'PBRMaterial') {
+    if (mesh.material?.getClassName() === 'PBRMaterial') {
       const mat = mesh.material as BABYLON.PBRMaterial;
       mat.metallic = 0;
     }
